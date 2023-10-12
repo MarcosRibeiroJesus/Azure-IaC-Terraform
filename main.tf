fae24d80c8ae2730d3735da11e73178f49b58e8c
EXTU <marcos.r.jesus.prestserv@petrobras.com.br>
2023-10-12 18:11:38 -0300

    Add "Set" Permission for Key Vault and Secrets

--- main.tf
+++ main.tf
@@ -53,11 +53,11 @@ resource "azurerm_key_vault" "xpe" {
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
-      "Get",
+      "Get", "Set"
     ]
 
     secret_permissions = [
-        "Get", "List"
+        "Get", "List", "Set"
         ]
 
     storage_permissions = [
