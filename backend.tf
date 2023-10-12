2f27ec850fc39849368e084f0d27edcfd80b9286
EXTU <marcos.r.jesus.prestserv@petrobras.com.br>
2023-10-12 18:37:14 -0300

    Set storage and resource group variables hardcoded

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 # Azure Storage Backend for Terraform state
 terraform {
   backend "azurerm" {
-    resource_group_name   = azurerm_resource_group.xpe.name
-    storage_account_name   = azurerm_storage_account.tfstate.name
+    resource_group_name   = "myxpeResourceGroup"
+    storage_account_name   = "tfstatestorageaccount"
     container_name         = "tfstatecontainer"
     key                    = "terraform.tfstate"
   }
